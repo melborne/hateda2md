88b0c53f35a3895902063ca5c053a82033db75e4
kyoendo <postagie@gmail.com>
2012-05-01 23:25:51 +0900

    add String extensions add save_to_files method make run method to take limit argument

--- lib/hateda2md/mdbuilder.rb
+++ lib/hateda2md/mdbuilder.rb
@@ -1,5 +1,15 @@
 # encoding: UTF-8
 require "nokogiri"
+class String
+  def ~
+    mergin = scan(/^ +/).map(&:size).min
+    gsub(/^ {#{mergin}}/, '')
+  end
+
+  def to_nil
+    self.empty? ? nil : self
+  end
+end
 
 class HateDa::MdBuilder
   attr_reader :entries
@@ -26,18 +36,68 @@ class HateDa::MdBuilder
     entries.each { |ent| ent.filter(pattern, opt, &replace) }
   end
 
+  def pre_defined_filters(alias_flag=false)
+    HateDa::Converter.pre_defined_filters(alias_flag)
+  end
   
-  def run
-    @entries.each do |entry|
+  def run(limit=nil)
+    limit ||= entries.size
+    entries.take(limit).map do |entry|
       md = entry.to_md(entry.ent_body)
       entry.ent_title = get_title(entry) if entry.ent_title.empty?
       entry.ent_mdbody = md
     end
   end
 
+  def save_to_files(dir='md', ext='md')
+    md_entries = entries.select { |ent| ent.ent_mdbody }
+    unless md_entries.empty?
+      Dir.mkdir(dir) unless Dir.exist?(dir)
+      md_entries.each do |ent|
+        path = "#{dir}/#{ent.ent_date}-#{title_for_file(ent.ent_title)}.#{ext}"
+        File.open(path, 'w') do |f|
+          f.puts header(ent.ent_title, ent.ent_date)
+          f.puts ent.ent_mdbody
+          f.puts footnote(ent.stocks[:footnote]) unless ent.stocks[:footnote].empty?
+        end
+      end
+    end
+  rescue => e
+    print "class => #{e.class}\nmessage => #{e.message}\nbacktrace => #{e.backtrace}\n"
+  end
+
   private
   def get_title(entry)
     entry.stocks[:titles].first || 'notitle'
+  end
+
+  def title_for_file(title)
+    title.scan(/\w+/).join('-').to_nil || 'notitle'
+  end
+
+  def header(title, date)
+    ~<<-EOS
+    ---
+    layout: post
+    title: #{title}
+    date: #{date}
+    comments: true
+    categories:
+    tags:
+    published: true
+    ---
+
+    EOS
+  end
+
+  def footnotes(fnotes)
+    notes = fnotes.map { |note| "  #{note}" }.join("\n")
+    ~<<-EOS
+    
+    {% footnotes %}
+    #{notes}
+    {% endfootnotes %}
+    EOS
   end
 end
 
